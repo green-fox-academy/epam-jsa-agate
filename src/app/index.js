556f2395ebe0dc3167b4b5f8c5a1c372b5330d8b
linjialiang1234 <linjialiang86@163.com>
2017-09-26 20:18:02 +0800

    AGT-4 change header content

--- src/app/index.js
+++ src/app/index.js
@@ -6,7 +6,7 @@ class ReactHeader extends React.Component{
   render() {
     return (
       <div>
-        <header>Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</header>
+        <header>Hello World!</header>
       </div>);
   }
 }
