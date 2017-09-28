027441165db8e50020ffc22e719afb449cc64402
Jerry-ZWL <yenmoon@icloud.com>
2017-09-28 11:36:28 +0800

    fix database avalability check

--- src/server/heart-beat-db.js
+++ src/server/heart-beat-db.js
@@ -3,22 +3,22 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/heartbeat';
 
-const checkDbHealth = function(res) {
+const checkDatabaseHealth = function(res) {
   MongoClient.connect(url, function(err, db) {
     if (err === null) {
       let collection = db.collection('heartbeat');
       collection.find({}).toArray(function(err, docs) {
-        if (err === null && docs && docs.length>0) {
-          res.json('{"status": "ok", "database": "ok"}');
+        if (err === null && docs.length>0) {
+          res.json({"status": "ok", "database": "ok"});
         } else {
-          res.json('{"status": "ok", "database": "error"}');
+          res.json({"status": "ok", "database": "error"});
         }
       });
     } else {
-      res.json('{"status": "ok", "database": "error"}');
+      res.json({"status": "ok", "database": "error"});
     }
     db.close();
   });
 };
 
-module.exports = checkDbHealth;
+module.exports = checkDatabaseHealth;
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+const checkDatabaseHealth = require('./heart-beat-db');
+
+app.get('/heartbeat', function(req, res) {
+  checkDatabaseHealth(res);
+  });
 
 app.listen(PORT, function() {
   console.log(`app is listening on port ${PORT}`);
