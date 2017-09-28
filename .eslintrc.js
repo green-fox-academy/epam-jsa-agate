f9742b9731aad3def30f870f9638893b48835b30
Jerry-ZWL <yenmoon@icloud.com>
2017-09-28 11:05:52 +0800

    refactor AGT-8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,3 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true
-    },
-    "sourceType": "module"
-  },
-  "extends": "google",
-  "parser": "babel-eslint",
-  "plugins": [
-    "react"
-  ],
-  "rules": {
-    "react/jsx-uses-react": "error",
-    "react/jsx-uses-vars": "error",
-    "require-jsdoc":0
-  }
+  "extends": "google"
 };
--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.listen(3449, function() {
-  console.log('epam-jsa-agate is running on port 3449!\n');
-});
