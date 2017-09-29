f310159fc130252acdb770cedc581c823a0fce00
Jerry-ZWL <yenmoon@icloud.com>
2017-09-29 09:35:18 +0800

    modify the callback

--- src/server/database-check.js
+++ src/server/database-check.js
@@ -3,36 +3,34 @@
 const config = require('../../.database-config.json');
 const MongoClient = require('mongodb').MongoClient;
 
-const address = config.database.url;
-const port = config.database.port;
-const databaseName = config.database.databaseName;
 const okStatus = {status: 'ok', database: 'ok'};
 const errorStatus = {status: 'ok', database: 'error'};
 
 const createDatabaseUrl = function () {
+  const address = config.database.url;
+  const port = config.database.port;
+  const databaseName = config.database.databaseName;
   return `${address}:${port}/${databaseName}`;
 };
 
-function databaseResponse(res, status) {
-  return function () {
-    res.json(status);
-  }();
-}
-
-const checkDatabaseHealth = function (res, callback) {
+const checkDatabaseHealth = function (callback) {
   const url = createDatabaseUrl();
   MongoClient.connect(url, function (err, db) {
+    let isWorking;
     if (err === null) {
       let collection = db.collection('heartbeat');
       collection.find({}).toArray(function (err, docs) {
         if (err === null && docs.length > 0) {
-          callback(res, okStatus);
+          isWorking = true;
+          callback(isWorking);
         } else {
-          callback(res, errorStatus);
+          isWorking = false;
+          callback(isWorking);
         }
       });
     } else {
-      callback(res, errorStatus);
+      isWorking = false;
+      callback(isWorking);
     }
     db.close();
   });
@@ -40,5 +38,6 @@ const checkDatabaseHealth = function (res, callback) {
 
 module.exports = {
   checkDatabaseHealth: checkDatabaseHealth,
-  databaseResponse: databaseResponse
+  okStatus: okStatus,
+  errorStatus: errorStatus
 };
--- src/server/index.js
+++ src/server/index.js
@@ -3,8 +3,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const DatabaseHealth = require('./database-check');
 
-app.get('/heartbeat', function(req, res) {
-  DatabaseHealth.checkDatabaseHealth(res,DatabaseHealth.databaseResponse);
+app.get('/heartbeat', function (req, res) {
+  DatabaseHealth.checkDatabaseHealth((isWorking) => {
+    isWorking ? res.json(DatabaseHealth.okStatus) :
+      res.json(DatabaseHealth.errorStatus);
+  });
 });
 
 app.listen(PORT, function() {
