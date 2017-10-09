0bca68ec7c49a188265a7fd91f5868f2e03f2bb2
linjialiang1234 <linjialiang86@163.com>
2017-10-09 15:00:40 +0800

    first draft

---
+++ src/client/app/component/BusinessOverview/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.scss';
+
+class BusinessOverview extends React.Component {
+  render() {
+  return (
+    <div id="business-overview-container">
+      <div id="single-business">
+        <span>***</span>
+        <span>flasdjflkjalkfjaslfj</span>
+      </div>
+      <h1><span>1.</span>okonamidjfjldsa chidhifadj</h1>
+      <p>fldajflkajlkfjaslfjasljfalkjfl;ksdsfaljasddlkjfasldkjf</p>
+      <div>
+        <p>#Nishi-nari Ward</p>
+        <button>More</button>
+      </div>
+    </div>
+  );
+  }
+}
+
+export default BusinessOverview;
