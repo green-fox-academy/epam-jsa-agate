dac4d30989abff6d1fde1976092eb52ba2a17f25
linjialiang1234 <linjialiang86@163.com>
2017-10-09 18:15:37 +0800

    businessOverview-draft3

--- src/client/app/component/BusinessOverview/index.js
+++ src/client/app/component/BusinessOverview/index.js
@@ -2,18 +2,28 @@ import React from 'react';
 import './style.scss';
 
 class BusinessOverview extends React.Component {
+  imagesDetails = [
+    {
+      business-rate : ***,
+
+    },
+    {
+
+    }
+  ];
+
   render() {
   return (
     <div id="business-overview-container">
-
       <div className="single-business">
         <div className="image-container">
           <span className="business-rate">***</span>
-          <span className="business-name">Epam-jsa-Agate</span>
+          <span className="business-name">EpamjsaAgate</span>
         </div>
 
-        <p className="business-title"><span>1.</span>Epam-jsa-Agate-titel</p>
-        <p className="business-description">business-description</p>
+        <p className="business-title"><span>1.</span>EpamjsaAgate-titel</p>
+        <p className="business-description">businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfld
+          afahddddddddddddd'ddddrjgtkrejtkFdd</p>
 
         <div className="business-infor">
           <span className="business-keywords">#Nishi-nari Ward</span>
