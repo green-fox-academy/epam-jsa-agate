68d548abfe2acbb4e8cde905687e5120f77a7832
linjialiang1234 <linjialiang86@163.com>
2017-10-09 20:18:03 +0800

    businessOverveiw-draf4

--- src/client/app/component/BusinessOverview/index.js
+++ src/client/app/component/BusinessOverview/index.js
@@ -2,34 +2,85 @@ import React from 'react';
 import './style.scss';
 
 class BusinessOverview extends React.Component {
-  imagesDetails = [
-    {
-      business-rate : ***,
-
-    },
-    {
-
-    }
-  ];
 
   render() {
+    var imagesDetails = [
+       {businessImageUrl: {background: `url(${require("./images/1.png")})`},
+       businssRate: '*****',
+       businessName: 'EpamjsaAgate',
+       businessTitle: 'EpamjsaAgate-titel',
+       businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+       businessKeywords: '#Nishi-nari Ward',
+       businessMore: 'More'},
+
+      //  {businessImage: 'url(./images/2.png)',
+      //  businssRate: '*****',
+      //  businessName: 'EpamjsaAgate',
+      //  businessTitle: 'EpamjsaAgate-titel',
+      //  businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+      //  businessKeywords: '#Nishi-nari Ward',
+      //  businessMore: 'More'},
+
+      //  {businessImage: 'url(./images/3.png)',
+      //  businssRate: '*****',
+      //  businessName: 'EpamjsaAgate',
+      //  businessTitle: 'EpamjsaAgate-titel',
+      //  businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+      //  businessKeywords: '#Nishi-nari Ward',
+      //  businessMore: 'More'},
+
+      //  {businessImage: 'url(./images/4.png)',
+      //  businssRate: '*****',
+      //  businessName: 'EpamjsaAgate',
+      //  businessTitle: 'EpamjsaAgate-titel',
+      //  businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+      //  businessKeywords: '#Nishi-nari Ward',
+      //  businessMore: 'More'},
+
+      //  {businessImage: 'url(./images/5.png)',
+      //  businssRate: '*****',
+      //  businessName: 'EpamjsaAgate',
+      //  businessTitle: 'EpamjsaAgate-titel',
+      //  businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+      //  businessKeywords: '#Nishi-nari Ward',
+      //  businessMore: 'More'},
+
+      //  {businessImage: 'url(./images/6.png)',
+      //  businssRate: '*****',
+      //  businessName: 'EpamjsaAgate',
+      //  businessTitle: 'EpamjsaAgate-titel',
+      //  businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+      //  businessKeywords: '#Nishi-nari Ward',
+      //  businessMore: 'More'},
+    ];
+
+    var allBusiness = [];
+
+     imagesDetails.map(
+      function(item, index) {
+        allBusiness.push(
+          <div className="single-business">
+          <div className="image-container" style={item.businessImageUrl} >
+            <span className="business-rate">{item.businssRate}</span>
+            <span className="business-name">{item.businessName}</span>
+          </div>
+  
+          <p className="business-title"><span>{index+1}</span>{item.businessTitle}</p>
+          <p className="business-description">{item.businessDescription}</p>
+  
+          <div className="business-infor">
+            <span className="business-keywords">{item.businessKeywords}</span>
+            <a className="business-more">{item.businessMore}</a>
+          </div>
+        </div>
+        );
+      },this);
+
+    
   return (
     <div id="business-overview-container">
-      <div className="single-business">
-        <div className="image-container">
-          <span className="business-rate">***</span>
-          <span className="business-name">EpamjsaAgate</span>
-        </div>
-
-        <p className="business-title"><span>1.</span>EpamjsaAgate-titel</p>
-        <p className="business-description">businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfld
-          afahddddddddddddd'ddddrjgtkrejtkFdd</p>
-
-        <div className="business-infor">
-          <span className="business-keywords">#Nishi-nari Ward</span>
-          <a className="business-more">More</a>
-        </div>
-      </div>
+      {allBusiness}
+     
     </div>
   );
   }
