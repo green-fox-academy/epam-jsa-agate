3916d8e3b3403b65a8399f54a34903274c0131ef
linjialiang1234 <linjialiang86@163.com>
2017-10-09 16:47:40 +0800

    businessOverview-draft2

--- src/client/app/component/BusinessOverview/index.js
+++ src/client/app/component/BusinessOverview/index.js
@@ -5,15 +5,20 @@ class BusinessOverview extends React.Component {
   render() {
   return (
     <div id="business-overview-container">
-      <div id="single-business">
-        <span>***</span>
-        <span>flasdjflkjalkfjaslfj</span>
-      </div>
-      <h1><span>1.</span>okonamidjfjldsa chidhifadj</h1>
-      <p>fldajflkajlkfjaslfjasljfalkjfl;ksdsfaljasddlkjfasldkjf</p>
-      <div>
-        <p>#Nishi-nari Ward</p>
-        <button>More</button>
+
+      <div className="single-business">
+        <div className="image-container">
+          <span className="business-rate">***</span>
+          <span className="business-name">Epam-jsa-Agate</span>
+        </div>
+
+        <p className="business-title"><span>1.</span>Epam-jsa-Agate-titel</p>
+        <p className="business-description">business-description</p>
+
+        <div className="business-infor">
+          <span className="business-keywords">#Nishi-nari Ward</span>
+          <a className="business-more">More</a>
+        </div>
       </div>
     </div>
   );
