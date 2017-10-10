c0f769f9a4634b1ce3fe853c3fb98f1203477c2c
Jerry-ZWL <yenmoon@icloud.com>
2017-10-10 11:55:29 +0800

    add business overview

--- src/client/app/component/HomePageContainer/index.js
+++ src/client/app/component/HomePageContainer/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BusinessOverview from '../BusinessOverview';
 import './style.scss';
 
 class HomePageContainer extends React.Component {
   render() {
     return (
       <div className = "HomePageContainer">
+        <BusinessOverview />
       </div>
     );
   }
