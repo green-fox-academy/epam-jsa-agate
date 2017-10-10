96376bf4311c982db0f84ccffbad7b7fceaaf199
linjialiang1234 <linjialiang86@163.com>
2017-10-10 18:21:56 +0800

    a

--- src/client/app/component/HomePageContainer/index.js
+++ src/client/app/component/HomePageContainer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavigationBar from '../NavigationBar';
 import BusinessOverview from '../BusinessOverview';
 import './style.scss';
 
@@ -6,6 +7,7 @@ class HomePageContainer extends React.Component {
   render() {
     return (
       <div className = "HomePageContainer">
+        <NavigationBar />
         <BusinessOverview />
       </div>
     );
---
+++ src/client/app/component/NavigationBar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.scss';
+
+class NavigationBar extends React.Component {
+  
+  render() {
+
+  return (
+    <nav id="nav-container">
+      <a className="nav-link" href="#">OVERVIEW</a>
+      <a className="nav-link" href="#">RESTARUANTS</a>
+      <a className="nav-link" href="#">NIGHTLIFE</a>
+      <a className="nav-link" href="#">HOME SERVICE</a>
+      <a className="nav-link" id="comment-img" href="#">WRITE A REVIEW</a> 
+      <img src="../src/client/app/component/Navigation/images/edit.png"/>   
+    </nav>
+  );
+  }
+}
+
+export default NavigationBar;
