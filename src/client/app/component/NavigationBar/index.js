36b99b175467f315981ed62dd24a605766e2886b
linjialiang1234 <linjialiang86@163.com>
2017-10-11 00:13:16 +0800

    fix navbar

--- src/client/app/component/NavigationBar/index.js
+++ src/client/app/component/NavigationBar/index.js
@@ -2,17 +2,27 @@ import React from 'react';
 import './style.scss';
 
 class NavigationBar extends React.Component {
-  
+  constructor(){
+    super();
+    this.state = {
+      addClass: -1
+    }
+  }
+
+  handleClickLink(event){
+    this.setState({addClass:event.target.id})
+  }
+ 
   render() {
 
   return (
     <nav id="nav-container">
-      <a className="nav-link" href="#">OVERVIEW</a>
-      <a className="nav-link" href="#">RESTARUANTS</a>
-      <a className="nav-link" href="#">NIGHTLIFE</a>
-      <a className="nav-link" href="#">HOME SERVICE</a>
-      <a className="nav-link" id="comment-img" href="#">WRITE A REVIEW</a> 
-      <img src="../src/client/app/component/Navigation/images/edit.png"/>   
+      <a href="#" id="1-link" className={this.state.addClass !== "1-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>OVERVIEW</a>
+      <a href="#" id="2-link" className={this.state.addClass !== "2-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>RESTARUANTS</a>
+      <a href="#" id="3-link" className={this.state.addClass !== "3-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>NIGHTLIFE</a>
+      <a href="#" id="4-link" className={this.state.addClass !== "4-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>HOME SERVICE</a>
+      <a href="#" id="5-link" className={this.state.addClass !== "5-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>
+      WRITE A REVIEW  <img id="comment-img" src="./images/edit.png"/></a> 
     </nav>
   );
   }
