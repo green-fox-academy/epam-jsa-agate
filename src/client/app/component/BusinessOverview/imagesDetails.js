05a8e5e1c2f2ddedaedf389c4dc1a353e541e0d3
linjialiang1234 <linjialiang86@163.com>
2017-10-11 14:25:53 +0800

    fake data in separate file

---
+++ src/client/app/component/BusinessOverview/imagesDetails.js
@@ -0,0 +1,65 @@
+export const imagesDetails = [
+  {
+   businessImageUrl: {
+     background: `url(${require("./images/1.png")})`},
+   businessScore: '*****',
+   businessName: 'EpamjsaAgate',
+   businessTitle: 'EpamjsaAgate-titel',
+   businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+   businessKeywords: '#Nishi-nari Ward',
+   businessMore: 'http://www.baidu.com'
+  },
+
+  {
+   businessImageUrl: {
+     background: `url(${require("./images/2.png")})`},
+   businessScore: '*****',
+   businessName: 'EpamjsaAgate',
+   businessTitle: 'EpamjsaAgate-titel',
+   businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+   businessKeywords: '#Nishi-nari Ward',
+   businessMore: 'http://www.baidu.com'
+  },
+
+  {
+   businessImageUrl: {
+     background: `url(${require("./images/3.png")})`},
+   businessScore: '*****',
+   businessName: 'EpamjsaAgate',
+   businessTitle: 'EpamjsaAgate-titel',
+   businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+   businessKeywords: '#Nishi-nari Ward',
+   businessMore: 'http://www.baidu.com'
+  },
+
+   {businessImageUrl: {background: `url(${require("./images/4.png")})`},
+   businessScore: '*****',
+   businessName: 'EpamjsaAgate',
+   businessTitle: 'EpamjsaAgate-titel',
+   businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+   businessKeywords: '#Nishi-nari Ward',
+   businessMore: 'http://www.baidu.com'
+  },
+
+  {
+   businessImageUrl: {
+     background: `url(${require("./images/5.png")})`},
+   businessScore: '*****',
+   businessName: 'EpamjsaAgate',
+   businessTitle: 'EpamjsaAgate-titel',
+   businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+   businessKeywords: '#Nishi-nari Ward',
+   businessMore: 'http://www.baidu.com'
+  },
+
+  {
+   businessImageUrl: {
+     background: `url(${require("./images/6.png")})`},
+   businessScore: '*****',
+   businessName: 'EpamjsaAgate',
+   businessTitle: 'EpamjsaAgate-titel',
+   businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
+   businessKeywords: '#Nishi-nari Ward',
+   businessMore: 'http://www.baidu.com'
+  },
+];
--- src/client/app/component/BusinessOverview/index.js
+++ src/client/app/component/BusinessOverview/index.js
@@ -1,76 +1,15 @@
 import React from 'react';
 import './style.scss';
 import SingleBusinessOverview from'../SingleBusinessOverview';
+import {imagesDetails} from'./imagesDetails';
 
+console.log("11" + imagesDetails)
 class BusinessOverview extends React.Component {
 
   render() {
-    var imagesDetails = [
-      {
-       businessImageUrl: {
-         background: `url(${require("./images/1.png")})`},
-       businessScore: '*****',
-       businessName: 'EpamjsaAgate',
-       businessTitle: 'EpamjsaAgate-titel',
-       businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
-       businessKeywords: '#Nishi-nari Ward',
-       businessMore: 'http://www.baidu.com'
-      },
-
-      {
-       businessImageUrl: {
-         background: `url(${require("./images/2.png")})`},
-       businessScore: '*****',
-       businessName: 'EpamjsaAgate',
-       businessTitle: 'EpamjsaAgate-titel',
-       businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
-       businessKeywords: '#Nishi-nari Ward',
-       businessMore: 'http://www.baidu.com'
-      },
-
-      {
-       businessImageUrl: {
-         background: `url(${require("./images/3.png")})`},
-       businessScore: '*****',
-       businessName: 'EpamjsaAgate',
-       businessTitle: 'EpamjsaAgate-titel',
-       businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
-       businessKeywords: '#Nishi-nari Ward',
-       businessMore: 'http://www.baidu.com'
-      },
-
-       {businessImageUrl: {background: `url(${require("./images/4.png")})`},
-       businessScore: '*****',
-       businessName: 'EpamjsaAgate',
-       businessTitle: 'EpamjsaAgate-titel',
-       businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
-       businessKeywords: '#Nishi-nari Ward',
-       businessMore: 'http://www.baidu.com'
-      },
-
-      {
-       businessImageUrl: {
-         background: `url(${require("./images/5.png")})`},
-       businessScore: '*****',
-       businessName: 'EpamjsaAgate',
-       businessTitle: 'EpamjsaAgate-titel',
-       businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
-       businessKeywords: '#Nishi-nari Ward',
-       businessMore: 'http://www.baidu.com'
-      },
-
-      {
-       businessImageUrl: {
-         background: `url(${require("./images/6.png")})`},
-       businessScore: '*****',
-       businessName: 'EpamjsaAgate',
-       businessTitle: 'EpamjsaAgate-titel',
-       businessDescription: 'businessdescriptionfdkssajflkdjalfjdaljfdlasjfljfaljfalfjlajfldafahdddddddddddddddddrjgtkrejtkFdd',
-       businessKeywords: '#Nishi-nari Ward',
-       businessMore: 'http://www.baidu.com'
-      },
-    ];
-
+    console.log(imagesDetails);
+    
+    
     var allBusiness = imagesDetails.map((item, index) => {
         item.businessTitle = index+1 + ". " + item.businessTitle;
         return <SingleBusinessOverview itemInfo={item} />        
