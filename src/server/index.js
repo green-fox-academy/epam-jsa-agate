11542333504553dfa55801b662f22543d7474aae
Levente Belteky <belteky.levente@gmail.com>
2017-10-11 18:14:19 +0800

    Revert "GET /api/businesses (#27)"
    This reverts commit a712abc6332e1ed09bfc769eb20d1e9b230636c7.

--- src/server/index.js
+++ src/server/index.js
@@ -1,40 +1,16 @@
 const express = require('express');
 const DatabaseHealth = require('./database-check');
-const BusinessessEndpoint = require('./business-endpoint');
 const path = require('path');
-const dbUtility = require('./db-utility');
-const businessesJson = require('./businesses.json');
 
-const collectionName = 'businesses';
-const dataFeedStatus = {insert: 'ok'};
 const app = express();
 const PORT = process.env.PORT || 3000;
 const okStatus = {status: 'ok', database: 'ok'};
 const errorStatus = {status: 'ok', database: 'error'};
 
-const apiErrorMessage = {error: 'something went wrong'};
-
-app.get('/feed', function(req, res) {
-  dbUtility.insertFileToDatabase(businessesJson, collectionName);
-  res.json(dataFeedStatus);
-});
-
 app.get('/heartbeat', function(req, res) {
   DatabaseHealth.checkDatabaseHealth((isWorking) => {
     isWorking ? res.json(okStatus) :
       res.json(errorStatus);
-  });
-});
-
-app.get('/api/businesses', function(req, res) {
-  BusinessessEndpoint.fetchBusinesses((isWorking, docs) => {
-    if (isWorking) {
-      let data = docs[0] ? docs[0].businesses : {};
-      let businesses = {businesses: data};
-      res.status(200).json(businesses);
-    } else {
-      res.status(500).json(apiErrorMessage);
-    }
   });
 });
 
