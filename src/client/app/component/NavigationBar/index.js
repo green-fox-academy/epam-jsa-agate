5b71d442c5726ce14d4c89a48f8542a62250c16a
Jerry-ZWL <yenmoon@icloud.com>
2017-10-11 09:29:49 +0800

    fix eslint

--- src/client/app/component/NavigationBar/index.js
+++ src/client/app/component/NavigationBar/index.js
@@ -2,29 +2,48 @@ import React from 'react';
 import './style.scss';
 
 class NavigationBar extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      addClass: -1
-    }
+      addClass: -1,
+    };
   }
 
-  handleClickLink(event){
-    this.setState({addClass:event.target.id})
+  handleClickLink(event) {
+    this.setState({addClass: event.target.id});
   }
 
   render() {
-
-  return (
-    <nav id="nav-container">
-      <a href="#" id="1-link" className={this.state.addClass !== "1-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>OVERVIEW</a>
-      <a href="#" id="2-link" className={this.state.addClass !== "2-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>RESTARUANTS</a>
-      <a href="#" id="3-link" className={this.state.addClass !== "3-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>NIGHTLIFE</a>
-      <a href="#" id="4-link" className={this.state.addClass !== "4-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>HOME SERVICE</a>
-      <a href="#" id="5-link" className={this.state.addClass !== "5-link"? "nav-link-origin":"nav-link-change"} onClick={this.handleClickLink.bind(this)}>
+    return (
+      <nav id="nav-container">
+        <a href="#" id="1-link"
+          className=
+            {this.state.addClass !== '1-link'?
+              'nav-link-origin':'nav-link-change'}
+          onClick={this.handleClickLink.bind(this)}>OVERVIEW</a>
+        <a href="#" id="2-link"
+          className=
+            {this.state.addClass !== '2-link'?
+              'nav-link-origin':'nav-link-change'}
+          onClick={this.handleClickLink.bind(this)}>RESTARUANTS</a>
+        <a href="#" id="3-link"
+          className=
+            {this.state.addClass !== '3-link'?
+              'nav-link-origin':'nav-link-change'}
+          onClick={this.handleClickLink.bind(this)}>NIGHTLIFE</a>
+        <a href="#" id="4-link"
+          className=
+            {this.state.addClass !== '4-link'?
+              'nav-link-origin':'nav-link-change'}
+          onClick={this.handleClickLink.bind(this)}>HOME SERVICE</a>
+        <a href="#" id="5-link"
+          className=
+            {this.state.addClass !== '5-link'?
+              'nav-link-origin':'nav-link-change'}
+          onClick={this.handleClickLink.bind(this)}>
       WRITE A REVIEW  <img id="comment-img" src="./images/edit.png"/></a>
-    </nav>
-  );
+      </nav>
+    );
   }
 }
 
