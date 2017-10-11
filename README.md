3d780713f5e7ff3dfc82291e1d6c2698c3fcc6f1
Levente Belteky <belteky.levente@gmail.com>
2017-10-11 05:14:35 -0500

    Revert "GET /api/businesses (#27)" (#30)
    This reverts commit a712abc6332e1ed09bfc769eb20d1e9b230636c7.

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ EPAM-JSA-AGATE --Local Business Finder (Yelp)
 npm run test:backend
 
 # configuration 
-**mongodb** create `.env` in root
-use .env.sample as template
+**mongodb** create `.application-config.json` in root
+use .application-config.sample as template
--- src/server/database-check.js
+++ src/server/database-check.js
@@ -1,10 +1,24 @@
 'use strict';
 
 const MongoClient = require('mongodb').MongoClient;
-const dbUtility = require('./db-utility');
+
+const config = function() {
+  if (process.env.NODE_ENV !== 'production') {
+    return require('../../.application-config.json');
+  } else {
+    return {};
+  }
+}();
+
+const createDatabaseUrl = function() {
+  const address = config.database.url || process.env.MONGO_ADDRESS;
+  const port = config.database.port || process.env.MONGO_PORT;
+  const databaseName = config.database.databaseName || process.env.MONGO_DBNAME;
+  return `${address}:${port}/${databaseName}`;
+};
 
 const checkDatabaseHealth = function(callback) {
-  const url = dbUtility.createDatabaseUrl();
+  const url = createDatabaseUrl();
   MongoClient.connect(url, function(err, db) {
     if (err === null) {
       let collection = db.collection('heartbeat');
@@ -16,7 +30,6 @@ const checkDatabaseHealth = function(callback) {
         }
       });
     } else {
-      console.log('err', err);
       callback(false);
     }
     db.close();
