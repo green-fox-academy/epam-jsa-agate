4d9012e0ae4fcee7073602f4b82787ff670a19e2
linjialiang1234 <linjialiang86@163.com>
2017-10-12 13:02:05 +0800

    DB

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const DatabaseHealth = require('./database-check');
+const DatabasePostRegister = require('./database-post-register');
 const BusinessessEndpoint = require('./business-endpoint');
 const path = require('path');
 const dbUtility = require('./db-utility');
 const businessesJson = require('./businesses.json');
 
+let bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+let jsonParser = bodyParser.json();
+
 const collectionName = 'businesses';
 const dataFeedStatus = {insert: 'ok'};
-const app = express();
 const PORT = process.env.PORT || 3000;
 const okStatus = {status: 'ok', database: 'ok'};
 const errorStatus = {status: 'ok', database: 'error'};
 const apiErrorMessage = {error: 'something went wrong'};
+const contentTypeError = {status: '400', description:
+'request\'s content-type header is not set to application/json'};
+const userNameMissing = {status: '400', description: 'username required'};
+const passWordMissing = {status: '400', description: 'password required'};
+const conflictUserName = {status: '409', description: 'conflict user name'};
+const otherError = {status: '500', description: 'something else went wrong'};
+const registerSuccess = {status: '201'};
 
 app.get('/feed', function(req, res) {
   dbUtility.insertFileToDatabase(businessesJson, collectionName);
@@ -41,6 +54,36 @@ app.use(express.static(path.resolve(__dirname, '../../dist')));
 
 app.get(['/', '/login', '/register'], (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../dist/index.html'));
+});
+
+app.post('/api/register', jsonParser, function(req, res) {
+  if (req.headers['content-type'] !== 'application/json') {
+    res.json(contentTypeError);
+    return;
+  }
+  if (!req.body.username) {
+    res.status(400).send(userNameMissing);
+    return;
+  }
+  if (!req.body.password) {
+    res.json(passWordMissing);
+    return;
+  }
+
+  DatabasePostRegister.postRegister(req.body, (dbResponseStatus) => {
+    if (dbResponseStatus === '409') {
+      res.json(conflictUserName);
+      return;
+    }
+    if (dbResponseStatus === '500') {
+      res.json(otherError);
+      return;
+    }
+    if (dbResponseStatus === '201') {
+      res.json(registerSuccess);
+      return;
+    }
+  });
 });
 
 app.listen(PORT, function() {
