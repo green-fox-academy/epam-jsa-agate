8305abedb20b0307de0c6951c51a4d11464bb3e4
Jessie Cai <tsai19930224@gmail.com>
2017-10-12 13:48:41 +0800

    fix navigation bar error

--- src/client/app/component/NavigationBar/index.js
+++ src/client/app/component/NavigationBar/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import './style.scss';
 
 class NavigationBar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      addClass: '1-link',
-    };
-  }
-
-  handleClickLink(event) {
-    this.setState({addClass: '1-link'});
-  }
-
   render() {
     return (
       <div>
@@ -20,32 +9,11 @@ class NavigationBar extends React.Component {
           <h1>Browsing Osaka</h1>
         </div>
         <nav id="nav-container">
-          <a href="#" id="1-link"
-            className=
-              {this.state.addClass !== '1-link'?
-                'nav-link-origin':'nav-link-change'}
-            onClick={this.handleClickLink.bind(this)}>OVERVIEW</a>
-          <a href="#" id="2-link"
-            className=
-              {this.state.addClass !== '2-link'?
-                'nav-link-origin':'nav-link-change'}
-            onClick={this.handleClickLink.bind(this)}>RESTARUANTS</a>
-          <a href="#" id="3-link"
-            className=
-              {this.state.addClass !== '3-link'?
-                'nav-link-origin':'nav-link-change'}
-            onClick={this.handleClickLink.bind(this)}>NIGHTLIFE</a>
-          <a href="#" id="4-link"
-            className=
-              {this.state.addClass !== '4-link'?
-                'nav-link-origin':'nav-link-change'}
-            onClick={this.handleClickLink.bind(this)}>HOME SERVICE</a>
-          <a href="#" id="5-link"
-            className=
-              {this.state.addClass !== '5-link'?
-                'nav-link-origin':'nav-link-change'}
-            onClick={this.handleClickLink.bind(this)}>
-            WRITE A REVIEW
+          <a href="#" id="1-link" className= 'nav-link-change'>OVERVIEW</a>
+          <a href="#" id="2-link" className= 'nav-link-origin'>RESTARUANTS</a>
+          <a href="#" id="3-link" className= 'nav-link-origin'>NIGHTLIFE</a>
+          <a href="#" id="4-link" className= 'nav-link-origin'>HOME SERVICE</a>
+          <a href="#" id="5-link" className= 'nav-link-origin'>WRITE A REVIEW
             <img id="comment-img" src="./images/edit.png"/></a>
         </nav>
       </div>
