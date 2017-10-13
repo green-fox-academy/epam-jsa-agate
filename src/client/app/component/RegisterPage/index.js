228745fa8351ba5c085b47e72aab18d09ca9a03e
linjialiang1234 <linjialiang86@163.com>
2017-10-13 13:48:56 +0800

    update

--- src/client/app/component/RegisterPage/index.js
+++ src/client/app/component/RegisterPage/index.js
@@ -4,12 +4,55 @@ import Header from '../CommonHeader';
 import './style.scss';
 
 class RegisterPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+  
+  submitHandle(event) {
+    event.preventDefault();
+    let postUserName = event.target.elements[0].value;
+    let postPassword = event.target.elements[1].value;
+    let retypePassword = event.target.elements[2].value;
+    if (postPassword !== retypePassword) {
+      alert('this two password are not the same');
+    } else {
+      let postData = {
+        'username': postUserName,
+        'password': postPassword,
+      };
+      let jSONPostData = JSON.stringify(postData);
+      let xhr = new XMLHttpRequest();
+      xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          let result = JSON.parse(xhr.responseText);
+          console.log(result);
+          if (result.status === '201') {
+            window.location.href = '/login';
+          }
+        }
+      });
+      xhr.open('POST', '/api/register', true);
+      this.setState({'status': 'loading'});
+      xhr.setRequestHeader('Accept', 'application/json');
+      xhr.setRequestHeader('content-type', 'application/json');
+      xhr.send(jSONPostData);
+    }
+  }
+
+  clickLogin(event) {
+    window.location.href = '/login';
+  }
   render() {
     return (
       <div className="register-page">
         <Header />
         <div className="register-page-content">
-          <RegisterForm />
+          <RegisterForm onSubmit={this.submitHandle.bind(this)}
+            onClickLogin={this.clickLogin.bind(this)}/>
         </div>
       </div>
     );
