91b9060118d27a4015e3e5bc2badd0a795ec0034
linjialiang1234 <linjialiang86@163.com>
2017-10-16 17:47:10 +0800

    copy registerForm

--- src/client/app/component/RegisterForm/index.js
+++ src/client/app/component/RegisterForm/index.js
@@ -5,19 +5,48 @@ class RegisterForm extends React.Component {
   constructor(props) {
     super(props);
   }
+  renderSpinnerButton() {
+    return (
+      <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+        width="40px" height="40px" viewBox="0 0 50 50" >
+        <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
+          <animateTransform attributeType="xml"
+            attributeName="transform"
+            type="rotate"
+            from="0 25 25"
+            to="360 25 25"
+            dur="0.6s"
+            repeatCount="indefinite"/>
+        </path>
+      </svg>
+    );
+  }
+  renderRegisterButton() {
+    return (
+      <input type="submit" value= 'Register Me!'/>
+    );
+  }
   render() {
     return (
       <div className="register-box">
         <h1>Sign up</h1>
-        <form>
-          <input type="email" name="email" required placeholder="E-mail" />
-          <input type="password" name="password" required
-            placeholder="Password"/>
-          <input type="password" name="retype-password" required
-            placeholder="Re-type Password"/>
-          <input type="submit" value="Register me!"/>
+        <div>
+          {this.props.warningInfo}
+        </div>
+        <form onSubmit={this.props.onSubmit}>
+          <input title="Username must be 3 to 15 letters" type="text"
+            name="username" required placeholder="Username"
+            pattern="[A-Za-z]{3,15}"/>
+          <input title="Password must be 6-8 letters or numbers" type="password"
+            name="password" required placeholder="Password"
+            pattern="[A-Za-z0-9]{6,8}" />
+          <input title="Password must be 6-8 letters or numbers" type="password"
+            name="retype-password" required
+            placeholder="Re-type Password" pattern="[A-Za-z0-9]{6,8}"/>
+          {this.props.isLoading ? this.renderSpinnerButton() :
+            this.renderRegisterButton()}
         </form>
-        <button>Log in</button>
+        <button type="button" onClick={this.props.onClickLogin()}>Log in</button>
       </div>
     );
   }
--- src/client/app/component/RegisterPage/index.js
+++ src/client/app/component/RegisterPage/index.js
@@ -1,15 +1,87 @@
 import React from 'react';
+import {Redirect} from 'react';
 import RegisterForm from '../RegisterForm';
 import Header from '../CommonHeader';
 import './style.scss';
 
 class RegisterPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      status: 'being',
+      warning: false,
+      warningMessage: '',
+      successLogin: false,
+    };
+  }
+  resetState() {
+    this.setState({warning: false});
+    this.setState({warningMessage: ''});
+  }
+  showPasswordError() {
+    this.setState({warning: true});
+    this.setState({warningMessage: 'These two passwords are not the same.'});
+  }
+  register(postData) {
+    let that = this;
+    let jSONPostData = JSON.stringify(postData);
+    let xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', function() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        let result = JSON.parse(xhr.responseText);
+
+        if (result.status === '409') {
+          that.setState({warning: true});
+          that.setState({warningMessage: 'User name conflict.'});
+        }
+        if (result.status === '201') {
+          window.location.href = '/login';
+        }
+        that.setState({'status': 'being'});
+      }
+    });
+    xhr.open('POST', '/api/register', true);
+    this.setState({'status': 'loading'});
+    xhr.setRequestHeader('Accept', 'application/json');
+    xhr.setRequestHeader('content-type', 'application/json');
+    this.setState({'status': 'loading'});
+    xhr.send(jSONPostData);
+  }
+  submitHandle(event) {
+    this.resetState();
+    event.preventDefault();
+
+    let postUserName = event.target.elements[0].value;
+    let postPassword = event.target.elements[1].value;
+    let retypePassword = event.target.elements[2].value;
+
+    if (postPassword !== retypePassword) {
+      this.showPasswordError();
+    } else {
+      let postData = {
+        'username': postUserName,
+        'password': postPassword,
+      };
+
+      this.register(postData);
+    }
+  }
+  clickLogin() {
+    // window.location.href = '/login';
+    // if (this.state.successLogin) {
+    return (<Redirect to="/login" />);
+    // }
+  }
   render() {
-    return (
+    return this.state.successLogin ? <Redirect to="/" /> : (
       <div className="register-page">
         <Header />
         <div className="register-page-content">
-          <RegisterForm />
+          <RegisterForm onSubmit={this.submitHandle.bind(this)} warningInfo={this.state.warningMessage}
+            isLoading={this.state.status === 'loading'} onClickLogin={this.clickLogin} />
         </div>
       </div>
     );
