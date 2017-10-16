e3728eb86eaa387be1c6b8009996a757ca2d4d53
Jessie Cai <tsai19930224@gmail.com>
2017-10-16 14:59:46 +0800

    login page funtionality

--- src/client/app/component/LoginForm/index.js
+++ src/client/app/component/LoginForm/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import {Spin} from 'antd';
+import 'antd/lib/spin/style/index.css';
 import './style.scss';
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {loading: false};
   }
   render() {
     return (
       <div className="login-box">
-        <h1>Log in</h1>
-        <form>
-          <input type="email" name="email" required placeholder="E-mail" />
-          <input type="password" name="password" required
-            placeholder="Password"/>
-          <input type="submit" value="login"/>
-        </form>
-        <button>Register</button>
+        <Spin spinning={this.props.loading}>
+          <h1>Log in</h1>
+          <form method="post" onSubmit={this.props.onSubmit}>
+            <label className="form-error-message"></label>
+            <input name="username" required placeholder="Username" />
+            <input type="password" name="password" required
+              placeholder="Password"/>
+            <input type="submit" value="login"/>
+          </form>
+          <button onClick={this.goToRegisterPage}>Register</button>
+        </Spin>
       </div>
     );
   }
--- src/client/app/component/LoginPage/index.js
+++ src/client/app/component/LoginPage/index.js
@@ -1,15 +1,80 @@
 import React from 'react';
+import {Redirect} from 'react-router-dom';
 import LoginForm from '../LoginForm';
 import Header from '../CommonHeader';
 import './style.scss';
 
 class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'loading': false,
+      'form': undefined,
+      'isLoggedIn': document.cookie.length > 0,
+    };
+  }
+  submitHandler(event) {
+    this.setState({'loading': true, 'form': event.target});
+    event.preventDefault();
+    this.submitData({
+      username: event.target.elements[0].value,
+      password: event.target.elements[1].value,
+    });
+  }
+  errorHandler(err) {
+    document.getElementsByClassName('form-error-message')[0].innerText = err;
+    for (let i = 0; i < 2; i++) {
+      let element = this.state.form.elements[i];
+
+      if (element.className.indexOf('login-form-error') === -1) {
+        this.state.form.elements[i].className += ' login-form-error';
+      }
+    }
+  }
+  removeErrorHandler() {
+    document.getElementsByClassName('form-error-message')[0].innerText = '';
+    for (let i = 0; i < 2; i++) {
+      let element = this.state.form.elements[i];
+
+      if (element.className.indexOf('login-form-error') > -1) {
+        element.className = element.className.replace(
+          (new RegExp('(^|\\s)' + 'login-form-error' + '(\\s|$)')), ' ');
+      }
+    }
+  }
+  submitData(data) {
+    let that = this;
+    let myHeaders = new Headers();
+    let myInit = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: myHeaders,
+    };
+
+    myHeaders.append('Content-Type', 'application/json');
+
+    fetch('/api/login', myInit).then(function(response) {
+      return response.json();
+    }).then(function(value) {
+      that.setState({loading: false});
+      if (value.error) {
+        throw new Error(value.error);
+      } else {
+        document.cookie = 'Authorization=' + value.token;
+        that.removeErrorHandler();
+        that.setState({'isLoggedIn': true});
+      }
+    }).catch(function(err) {
+      that.errorHandler(err);
+    });
+  }
   render() {
-    return (
+    return this.state.isLoggedIn ? (<Redirect to="/" />) : (
       <div className="login-page">
         <Header />
         <div className="login-page-content">
-          <LoginForm />
+          <LoginForm onSubmit={this.submitHandler.bind(this)}
+            loading={this.state.loading}/>
         </div>
       </div>
     );
