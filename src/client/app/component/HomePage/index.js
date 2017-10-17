c9b17cd4a5d4b5f84f7e05d2a92c08753c4b4ec9
Jerry-ZWL <yenmoon@icloud.com>
2017-10-17 09:36:34 +0800

    init

--- src/client/app/component/HomePage/index.js
+++ src/client/app/component/HomePage/index.js
@@ -31,7 +31,7 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className = "home-page">
-        <HomePageHeader/>
+        <HomePageHeader businesses = {this.state.businesses}/>
         <div className = "main">
           <HomePageContainer businesses={this.state.businesses}/>
           <HomePageMap businesses={this.state.businesses}/>
