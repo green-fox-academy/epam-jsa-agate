80a3063472316cf26dde130c29723534513b51af
Jerry-ZWL <yenmoon@icloud.com>
2017-10-17 10:06:50 +0800

    error message to sentence

--- src/server/responseMessage.js
+++ src/server/responseMessage.js
@@ -8,10 +8,10 @@ const responseMessage = {
     status: '400', description:
   'request\'s content-type header is not set to application/json',
   },
-  USERNAME_MISSING: {status: '400', description: 'username required'},
-  PASSWORD_MISSING: {status: '400', description: 'password required'},
-  USERNAME_CONFLICT: {status: '409', description: 'conflict user name'},
-  OTHER_ERROR: {status: '500', description: 'something else went wrong'},
+  USERNAME_MISSING: {status: '400', description: 'Username required.'},
+  PASSWORD_MISSING: {status: '400', description: 'Password required.'},
+  USERNAME_CONFLICT: {status: '409', description: 'Conflict user name.'},
+  OTHER_ERROR: {status: '500', description: 'Something else went wrong.'},
   REGISTER_SUCCESS: {status: '201'},
 };
 
