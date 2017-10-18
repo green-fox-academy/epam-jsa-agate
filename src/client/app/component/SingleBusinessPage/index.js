642063b0b9d09ff790d85b0d86e3ee2742c7c804
Jerry-ZWL <yenmoon@icloud.com>
2017-10-18 10:58:28 +0800

    single business page structure

--- src/client/app/component/SingleBusinessPage/index.js
+++ src/client/app/component/SingleBusinessPage/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
+import Header from '../HomePageHeader';
 import './style.scss';
 
-class SingleBusinessPage extends  React.Component {
-
+class SingleBusinessPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {'name': this.props.param.name};
+  }
+  render() {
+    return (
+      <div className="single-business-page">
+        <Header/>
+        <DisplayBusiness/>
+        <CommentList/>
+      </div>
+    );
+  }
 }
