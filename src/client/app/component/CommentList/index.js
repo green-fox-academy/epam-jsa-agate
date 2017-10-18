1ea09155fb851eaa21563797c4fec89b358b1273
Jerry-ZWL <yenmoon@icloud.com>
2017-10-18 15:44:47 +0800

    placehoder for every component

---
+++ src/client/app/component/CommentList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+class CommentList extends React.Component {
+  render() {
+    return (
+      <div className="comment-list"></div>
+    );
+  }
+}
+
+export default CommentList;
+
---
+++ src/client/app/component/DisplayImage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+class DisplayImage extends React.Component {
+  render() {
+    return (
+      <div className="display-image"></div>
+    );
+  }
+}
+
+export default DisplayImage;
