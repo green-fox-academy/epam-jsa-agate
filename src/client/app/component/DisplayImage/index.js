d842d9a6ddcabb0c61e04f28a89d5ccc60940159
Jerry-ZWL <yenmoon@icloud.com>
2017-10-19 10:14:39 +0800

    part 1

--- src/client/app/component/DisplayImage/index.js
+++ src/client/app/component/DisplayImage/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './style.scss';
+import * as imagesJson from './imagesForTest.json';
+
+const imagesList = imagesJson.imagesUrl;
+const imageData =
+  [imagesList[2], imagesList[3], imagesList[4], imagesList[5]];
 
 class DisplayImage extends React.Component {
   render() {
     return (
-      <div className="display-image"></div>
+      <div className="display-image">
+        <img className="slot one" src={imagesList[0]}/>
+        <img className="slot two" src={imagesList[1]}/>
+        <img className="slot three" src={imagesList[2]}/>
+      </div>
     );
   }
 }
