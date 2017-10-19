1fc45f494ad53bf9a74515d897db5cf07e8271c7
Jessie Cai <tsai19930224@gmail.com>
2017-10-19 14:20:16 +0800

    add fetch function for detail page

--- src/client/app/component/SingleBusinessPage/index.js
+++ src/client/app/component/SingleBusinessPage/index.js
@@ -5,6 +5,9 @@ import HomePageHeader from '../HomePageHeader';
 import HomePageMap from '../HomePageMap';
 import DisplayImage from '../DisplayImage';
 import CommentList from '../CommentList';
+import notification from 'antd/lib/notification';
+
+import 'antd/lib/notification/style/index.css';
 import './style.scss';
 
 class SingleBusinessPage extends React.Component {
@@ -12,7 +15,31 @@ class SingleBusinessPage extends React.Component {
     super(props);
     this.state = {'id': this.props.match.params.id};
   }
+  componentWillMount() {
+    this.fetchBusinessesDetail();
+  }
+  errorHandler(err) {
+    notification.open({
+      message: err.message,
+      description: 'Please try again.',
+      placement: 'bottomLeft',
+    });
+  }
+  fetchBusinessesDetail() {
+    let that = this;
 
+    fetch('/api/business/' + that.state.id).then(function(response) {
+      return response.json();
+    }).then(function(value) {
+      if (value.error) {
+        throw new Error(value.error);
+      } else {
+        console.log(value);
+      }
+    }).catch(function(err) {
+      that.errorHandler(err);
+    });
+  }
   render() {
     return (
       <div className="single-business-page">
