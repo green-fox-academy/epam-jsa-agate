c6cde28f395476d8badad59de37bb29200c0eb52
linjialiang1234 <linjialiang86@163.com>
2017-10-26 18:15:19 +0800

    upload images

--- src/client/app/component/CreatingNewBusinessForm/index.js
+++ src/client/app/component/CreatingNewBusinessForm/index.js
@@ -4,6 +4,15 @@ import Spin from 'antd/lib/spin';
 import 'antd/lib/spin/style/index.css';
 import './style.scss';
 
+import Button from 'antd/lib/button';
+import 'antd/lib/button/style/index.css';
+
+import Upload from 'antd/lib/upload';
+import 'antd/lib/upload/style/index.css';
+
+import message from 'antd/lib/message';
+import 'antd/lib/message/style/index.css';
+
 class CreatingNewBusinessForm extends React.Component {
   constructor(props) {
     super(props);
@@ -14,8 +23,72 @@ class CreatingNewBusinessForm extends React.Component {
       this.setState({address: nextProps.address});
     }
   }
+
+  getSignedRequest(file) {
+    return fetch(`${serverUrl}/sign-s3?fileName=${file.name}&fileType=${file.type}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+      });
+  }
+
+  uploadFile(file, signedRequest, url) {
+    const options = {
+      method: 'PUT',
+      body: file,
+    };
+
+    return fetch(signedRequest, options)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        }
+        return url;
+      });
+  }
+
+  uploadToS3(file) {
+    return getSignedRequest(file)
+      .then((json) => uploadFile(file, json.signedRequest, json.url))
+      .then((url) => url)
+      .catch((err) => {
+        console.error(err);
+        return null;
+      });
+  }
+
+  handleImageSubmit() {
+    const input = document.querySelector("#file-input");
+    if (!input.files || !input.files.length) {
+      return;
+    }
+    const file = input.files[0];
+  
+    uploadToS3(file)
+      .then(url => {
+        // save the url to the database
+      });
+  }
   render() {
     const {loading, onSubmit, address} = this.props;
+    const props = {
+      name: 'file',
+      action: '//jsonplaceholder.typicode.com/posts/',
+      headers: {authorization: 'authorization-text' },
+
+      onChange(info) {
+        if (info.file.status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully`);
+        } else if (info.file.status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
 
     return (
       <div className="creating-new-business-form">
@@ -42,9 +115,12 @@ class CreatingNewBusinessForm extends React.Component {
             <input name="key-words" id="business-key-words" required
               type="text" placeholder="Coffee Asian ..."/>
             <label htmlFor="image-url">Images url</label>
-            <textarea id="image-url" rows="3" placeholder=
+            {/* <textarea id="image-url" rows="3" placeholder=
               "Highly recommend to input 3 images and seperate them into 3 lines."
-            required></textarea>
+            required></textarea> */}
+            <Upload {...props}>
+              <Button>Click to Upload</Button>
+            </Upload>
             <input className="business-submit"
               type="submit" value="Add business"/>
           </form>
