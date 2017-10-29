560ce25d7b10d715cb2e585f7e323a79e1382812
linjialiang1234 <linjialiang86@163.com>
2017-10-29 19:03:50 +0800

    create reposive page

--- src/client/app/component/CreatingNewCommentForm/index.js
+++ src/client/app/component/CreatingNewCommentForm/index.js
@@ -5,6 +5,7 @@ import 'antd/lib/spin/style/index.css';
 import Rate from 'antd/lib/rate';
 import 'antd/lib/rate/style/index.css';
 import './style.scss';
+// import SingleBusinessTitle from '../SingleBusinessTitle';
 
 class CreatingNewCommentForm extends React.Component {
   constructor(props) {
@@ -23,21 +24,32 @@ class CreatingNewCommentForm extends React.Component {
     return (
       <div className="creating-new-comment-form">
         <Spin spinning={loading}>
-          <div className="business-container">
-            <h1 >{this.props.businessDetail.name}</h1>
-            <div><h2>General Rating </h2> <Rate allowHalf character="★" disabled
-              value={this.props.businessDetail.rating} /></div>
+          <h3 className="comment-title">Write a comment</h3>
+          {/* <div className="business-container"> */}
+          {/* <h1 >{this.props.businessDetail.name}</h1> */}
+          {/* <div><h2>General Rating </h2> <Rate allowHalf character="★" disabled
+              value={this.props.businessDetail.rating} /></div> */}
+          {/* </div> */}
+          <div className="business-info">
+            <div className="business-info-name-rating">
+              <h3 className="comment-business-name">{this.props.businessDetail.name}</h3>
+              <p>Rating: {Number(this.props.businessDetail.rating).toFixed(2)}</p>
+            </div>
           </div>
           <form className="comment-info"
             method="POST" name="comment-info-form"
             onSubmit={onSubmit} onKeyPress = {this.handleKeyPress}>
-            <div className="rating-container">
-              <Rate allowHalf onChange={changeRating}
-                character="★" value={rating} />
-              <span className="ant-rate-text">(Please give rates for this business)</span>
+            <p className="comment-info-name">Your comment</p>
+            <div className="comment-container">
+              <div className="rating-container">
+                <Rate allowHalf onChange={changeRating}
+                  character="★" value={rating} />
+                <span className="ant-rate-text">Select your rating</span>
+              </div>
+              <textarea rows="4" cols="50" name="comment-input"
+                required placeholder="Add you comment" />
             </div>
-            <textarea rows="4" cols="50" name="comment-input"
-              required placeholder="Add you comment" />
+
             <input className="comment-submit"
               type="submit" value="Add Comment"/>
           </form>
