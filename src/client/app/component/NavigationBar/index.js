6265d845e4909f1d31e2da6dbc20b13a043782f3
Jerry-ZWL <yenmoon@icloud.com>
2017-10-29 21:19:05 +0800

    navigation change with css effect

--- src/client/app/component/NavigationBar/index.js
+++ src/client/app/component/NavigationBar/index.js
@@ -2,6 +2,49 @@ import React from 'react';
 import './style.scss';
 
 class NavigationBar extends React.Component {
+  categoryFilter(arrList) {
+    const that = this;
+    const map = Array.prototype.map;
+
+    map.call(arrList,
+      (element) => element.addEventListener('click', function(event) {
+        const category = element.innerText;
+
+        let removeClassNameFromListPromise = new Promise((resolve, reject) => {
+          resolve(that.removeClassNameFromList(arrList)());
+        });
+
+        removeClassNameFromListPromise.then(
+          (success) => {
+            element.classList.add('nav-link-change');
+            if (category !== 'OVERVIEW') {
+              that.props.navigation(category, 'category')();
+            } else {
+              that.props.navigation('', 'name')();
+            }
+          }
+        );
+      })
+
+    );
+  }
+  removeClassNameFromList(arrList) {
+    const map = Array.prototype.map;
+
+    return function() {
+      map.call(arrList,
+        (element) => element.classList.remove('nav-link-change'));
+    };
+  }
+
+  componentDidMount() {
+    const that = this;
+    const navContainer = document.getElementsByClassName('nav-container')[0];
+    const navLinkOrigin = navContainer.getElementsByTagName('a');
+
+    that.categoryFilter(navLinkOrigin);
+  }
+
   render() {
     return (
       <div className="home-page-navigation-container">
